d2591d6713a4be77cddc51d00c0311c921254ba8
Kazuhiro Homma <kazu.homma@gmail.com>
2013-04-29 21:42:18 +0900

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,82 @@
+class BooksController < ApplicationController
+  # 一番下のprivateメソッドのところにあるset_bookを
+  # show, edit, update, destroyを処理するときだけ
+  # 事前に実行する
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+
+  # GET /books
+  # GET /books.json
+  def index
+    # 本のレコード全部を表示する
+    @books = Book.all
+  end
+
+  # GET /books/1
+  # GET /books/1.json
+  def show
+    # id指定されたものを表示する
+    # なんと何も書かなくていいとは！
+  end
+
+  # GET /books/new
+  def new
+    @book = Book.new
+  end
+
+  # GET /books/1/edit
+  def edit
+  end
+
+  # POST /books
+  # POST /books.json
+  def create
+    @book = Book.new(book_params)
+
+    respond_to do |format|
+      # saveの成功判定をして分岐
+      if @book.save
+        format.html { redirect_to @book, notice: 'Book was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @book }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /books/1
+  # PATCH/PUT /books/1.json
+  def update
+    respond_to do |format|
+      if @book.update(book_params)
+        format.html { redirect_to @book, notice: 'Book was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /books/1
+  # DELETE /books/1.json
+  def destroy
+    @book.destroy
+    respond_to do |format|
+      format.html { redirect_to books_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_book
+      # idの指定があれば値が入る
+      @book = Book.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def book_params
+      params.require(:book).permit(:title, :price)
+    end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tetsuzine::Application.config.secret_key_base = '79bc78072d609ad92ac8e572a253225b22b4997422895490d101b111beaf43d66cbb6ab893324087a54d74a70bf2d8202b18f3da4279b8c686f3b87c97f50db6'
